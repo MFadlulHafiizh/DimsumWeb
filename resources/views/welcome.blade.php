802e91e8b6f29ec6d4011786ffed26243a4c7bf0
M Fadlul Hafiizh <muh.hafiizh56@gmail.com>
2020-09-02 15:38:51 +0700

    Introduction

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style type="text/css">
+        .bdr{
+            background-color:#E2D490;
+        }
+        .icon{
+            margin-top: 30px;
+            margin-left: 16px;
+        }
+        .info{
+            margin-top: 24px;
+        }
+        .font{
+            padding-top: 8px;
+        }
+    </style>
+    <title>Introduction</title>
+    <script type="text/javascript" src="assets/bootstrap/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/popper.min.js"></script>
+    <script type="text/javascript" src="assets/bootstrap/js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+</head>
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <a class="navbar-brand" href="#">
+            <img src="item/nav_ico64.png">
+            <span class="font">Dimsum Sadulur</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Introduction <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+        </ul>
+    </div>
+    </nav>
+
+<body class="bdr">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-5 icon">
+                <img src="item/icon_utama.png" class="img-fluid" alt="Dimsum Sadulur">
+            </div>
+            <div class="col-md-3 offset-md-3 icon">
+                <div id="carouselExampleIndicators" class="carousel slide ml-auto" data-ride="carousel">
+                      <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                      </ol>
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="item/menu1.jpeg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="item/menu2.jpeg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="item/menu3.jpeg" class="d-block w-100" alt="...">
+                        </div>
+                      </div>
+                      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+            </div>
+        </div>
+        <div class="col-md-6 info" style="margin: 24px;">
+            Sadulur dimsum berdiri pada Tahun 2017 dan dengan kerja keras kami akan selalu memberikan inovasi - inovasi olahan rasa yang berbeda dari produsen lain nya. Nama Sadulur Dimsum terinspirasi dari 80% yang bekerja sama adalah keluarga besar, mulai dari investasi, produksi dan penjualan serta managemen semua didukung oleh keluarga besar yang notabanenya adalah satu garis keturunan dan kami ingin menjadikan Produk Dimsum kami adalah produk Dimsum yang bisa turun menurun dan tak lekang oleh perputaran generasi.
+            <br>
+            <br>
+            <a href="#" class="btn btn-success">Lihat Lebih Lanjut</a>
+        </div>
+    </div>
+
+</body>
+</html>
+
+</body>
+</html>
