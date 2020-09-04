91f0f110693a64f8ab00e6a34142a96f39b68b7e
IrhamGhani22 <irhamag22@gmail.com>
2020-09-04 16:35:12 +0700

    Revisi Tampilan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <style type="text/css">
-        .bdr{
-            background-color:#E2D490;
-        }
-        .icon{
-            margin-top: 30px;
-            margin-left: 16px;
-        }
-        .info{
-            margin-top: 24px;
-        }
-        .font{
-            padding-top: 8px;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"></link>
     <title>Introduction</title>
     <script type="text/javascript" src="assets/bootstrap/js/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="assets/bootstrap/js/popper.min.js"></script>
     <script type="text/javascript" src="assets/bootstrap/js/bootstrap.js"></script>
-    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+
 </head>
     <nav class="navbar navbar-expand-lg navbar-light bg-warning">
         <a class="navbar-brand" href="#">
-            <img src="item/nav_ico64.png">
-            <span class="font">Dimsum Sadulur</span>
+            <img src="item/ico_nav_flat.png">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Introduction <span class="sr-only">(current)</span></a>
+        <ul class="navbar-nav text-dark mr-auto">
+            <li class="nav-item test">
+                <a class="nav-link font-weight-bolder aktif" href="#">INTRODUCTION <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link font-weight-bolder sliding-underline pb-2 ml-2" href="#">HOME</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link font-weight-bolder sliding-underline pb-2  ml-2" href="#">HELP</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link font-weight-bolder sliding-underline pb-2  ml-2" href="#">ABOUT</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link font-weight-bolder sliding-underline pb-2  ml-2" href="#">CONTACT</a>
+            </li>
+
         </ul>
     </div>
     </nav>
@@ -46,11 +42,16 @@
 <body class="bdr">
     <div class="col-md-12">
         <div class="row">
-            <div class="col-md-5 icon">
-                <img src="item/icon_utama.png" class="img-fluid" alt="Dimsum Sadulur">
+            <div class="col-md-6 icon1 p-5">
+                <h1 class="ha1">DIMSUM SADULUR</h1>
+                Sadulur dimsum berdiri pada Tahun 2017 dan dengan kerja keras kami akan selalu memberikan inovasi - inovasi olahan rasa yang berbeda dari produsen lain nya. Nama Sadulur Dimsum terinspirasi dari 80% yang bekerja sama adalah keluarga besar, mulai dari investasi, produksi dan penjualan serta managemen semua didukung oleh keluarga besar yang notabanenya adalah satu garis keturunan dan kami ingin menjadikan Produk Dimsum kami adalah produk Dimsum yang bisa turun menurun dan tak lekang oleh perputaran generasi.
+            <br>
+            <br>
+            <a href="#" class="btn btn-color mt-4 font-weight-bolder">LIHAT LEBIH LANJUT</a>
             </div>
-            <div class="col-md-3 offset-md-3 icon">
-                <div id="carouselExampleIndicators" class="carousel slide ml-auto" data-ride="carousel">
+            <div class="col-md-6 icon">
+                <img src="item/icon_utama.png" class="img-fluid" alt="Dimsum Sadulur">
+                {{-- <div id="carouselExampleIndicators" class="carousel slide ml-auto" data-ride="carousel">
                       <ol class="carousel-indicators">
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -75,15 +76,15 @@
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
                       </a>
-                    </div>
+                    </div> --}}
             </div>
         </div>
-        <div class="col-md-6 info" style="margin: 24px;">
+        {{-- <div class="col-md-6 info" style="margin: 24px;">
             Sadulur dimsum berdiri pada Tahun 2017 dan dengan kerja keras kami akan selalu memberikan inovasi - inovasi olahan rasa yang berbeda dari produsen lain nya. Nama Sadulur Dimsum terinspirasi dari 80% yang bekerja sama adalah keluarga besar, mulai dari investasi, produksi dan penjualan serta managemen semua didukung oleh keluarga besar yang notabanenya adalah satu garis keturunan dan kami ingin menjadikan Produk Dimsum kami adalah produk Dimsum yang bisa turun menurun dan tak lekang oleh perputaran generasi.
             <br>
             <br>
             <a href="#" class="btn btn-success">Lihat Lebih Lanjut</a>
-        </div>
+        </div> --}}
     </div>
 
 </body>
